7a6eb8799da4da1a6e808590e9e9b8c4c3f0c173
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-05-07 01:12:55 +0530

    Time: 301 ms (22.65%) | Memory: 182.2 MB (8.36%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -11,37 +11,30 @@
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-        stack<ListNode*> stack;
-        ListNode* current = head;
-
-        // Add nodes to the stack
-        while (current != nullptr) {
-            stack.push(current);
-            current = current->next;
+        // base case
+        if(head == NULL) return NULL;
+        stack<ListNode*>st;
+        ListNode *current = head;
+        // push vales in stack
+        while(current != NULL){
+            st.push(current);
+            current= current->next;
         }
-
-        current = stack.top();
-        stack.pop();
-        int maximum = current->val;
-        ListNode* resultList = new ListNode(maximum);
-
-        // Remove nodes from the stack and add to result
-        while (!stack.empty()) {
-            current = stack.top();
-            stack.pop();
-            // Current should not be added to the result
-            if (current->val < maximum) {
-                continue;
-            }
-            // Add new node with current's value to front of the result
-            else {
-                ListNode* newNode = new ListNode(current->val);
-                newNode->next = resultList;
-                resultList = newNode;
-                maximum = current->val;
+         current= st.top();
+         st.pop();
+         int maxi= current->val;
+        ListNode *result= new ListNode(current->val);
+        while(!st.empty()){
+            current = st.top();
+            st.pop();
+            if(current->val < maxi) continue;
+            else{
+                ListNode *newNode = new ListNode(current->val);
+                newNode->next = result;
+                result = newNode;
+                maxi= current->val;
             }
         }
-
-        return resultList;
+        return result;
     }
 };
