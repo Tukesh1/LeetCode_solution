19aafd410e9de6a7ec0843aef6e7c051da70e981
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2024-08-29 02:18:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (68.88%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -5,15 +5,11 @@ public:
          2 3 4 7 11
          1 5 6 8 9 10 12...
         */
-       int ind = 0, ans = 1, n = arr.size();
-        priority_queue<int> maxHeap;
-
-        while (maxHeap.size() != k) {
-            if ((ind < n && ans < arr[ind]) || ind >= n) maxHeap.push(ans);
-            if (ind < n && ans == arr[ind]) ind++;
-            ans++;
-        }
-        return maxHeap.top();
+      for(int i=0 ;i<arr.size();i++){
+        if(arr[i]<=k)k++;
+        else break;
+      }
+      return k;
 
 
     }
