80805285c6f5a677c14c32840f52c972b9181ad0
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2025-01-28 06:20:19 +0530

    Time: 4 ms (2.72%) | Memory: 15.2 MB (18.54%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,28 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    void helper(TreeNode* root,int level ,vector<int>&ans ){
-        if(root == nullptr) return;
-        if(ans.size() == level){
-            ans.push_back(root->val);
-        }
-            helper(root->right, level+1, ans);
-            helper(root->left, level+1, ans); 
-    }
     vector<int> rightSideView(TreeNode* root) {
-          vector<int>ans;
-          int level = 0;
-          helper(root, level , ans);
-          return ans;
+        //1. level order transversal
+        //2.BFS traversal 
+        if(root== NULL) return {};
+        vector<int>sol;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+           int n= q.size(); // size of queue
+            TreeNode *curr = nullptr;
+           while(n--){
+            curr = q.front();
+            q.pop();
+            if(curr->left != NULL) q.push(curr->left);
+            if(curr->right != NULL) q.push(curr->right);
+           }
+           sol.push_back(curr->val);
+        }
+        return sol;
     }
 };
