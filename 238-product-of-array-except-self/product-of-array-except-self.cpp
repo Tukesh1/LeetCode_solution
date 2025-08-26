71613d1d4673d8fdf7bed85ac56115eec39ceea1
Tukesh Kumar <115998898+Tukesh1@users.noreply.github.com>
2025-08-27 02:36:52 +0530

    Time: 3 ms (37.56%) | Memory: 42.3 MB (8.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-     int n = nums.size();
-    vector<int> left(n, 1), right(n, 1), ans(n);
-
-    for (int i = 1; i < n; ++i)
-        left[i] = left[i - 1] * nums[i - 1];
-
-    for (int i = n - 2; i >= 0; --i)
-        right[i] = right[i + 1] * nums[i + 1];
-
-    for (int i = 0; i < n; ++i)
-        ans[i] = left[i] * right[i];
-
-    return ans;
+        // left product and right product
+        int n = nums.size();
+        //[1,2,3,4]
+        vector<int> left(n, 1), right(n, 1), res(n, 1);
+        int prod = 1;
+        for (int i = 0; i < n; i++) {
+            prod = nums[i] * prod;
+            left[i] = prod;
+        }
+        // left = [1,2,6,24]
+        prod = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            prod = nums[i] * prod;
+            right[i] = prod;
+        }
+        // right = [24,12,8,6]
+        // res[0] = right[1]; res[n - 1] = left[n - 2]; for(I = 1 to n-1
+        // )res[i]= left[i-1]*right[i+1]
+        res[0] = right[1];
+        res[n - 1] = left[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            res[i]= left[i-1] *right[i+1];
+        }
+        return res;
     }
 };
