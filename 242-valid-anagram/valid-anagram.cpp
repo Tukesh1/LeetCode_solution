c08ab99ec3acb8f2b4c6e05729b25cb47a4b2b73
Tukesh Kumar <tukeshkrraju1011@gmail.com>
2025-09-15 18:05:33 +0530

    Time: 4 ms (39.52%) | Memory: 10.1 MB (11.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,9 +2,23 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
-        sort(s.begin(),s.end());
+       /* sort(s.begin(),s.end());
         sort(t.begin(), t.end());
         if(s!=t) return false;
+        return true;     //brute force solution
+        */
+
+
+        //using hashmap
+        unordered_map<char,int>s_mp;
+        unordered_map<char,int>t_mp;
+        for(int i=0;i<s.size();i++){
+            s_mp[s[i]]++;
+            t_mp[t[i]]++;
+        }
+        for (auto &p : s_mp) {
+            if (t_mp[p.first] != p.second) return false;
+        }
         return true;
     }
 };
